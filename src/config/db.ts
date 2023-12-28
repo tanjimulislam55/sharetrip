b587e60df6e382f7124950d3cbd5280204dac455
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-12-28 13:38:56 +0600

    mysql db configeration

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise'
+require('dotenv').config()
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+})
+
+export default conn
